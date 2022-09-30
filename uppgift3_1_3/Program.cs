05fe2c27cfccbb6d16d8a8abd2bcb2fad0a3b137
AdnanBukvic <muqa04001@utb.vaxjo.se>
2022-09-30 13:49:23 +0200

    Add project files.

---
+++ uppgift3_1_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >=16 && ålder <=19)
+        {
+             Console.WriteLine("Du får delta");
+        }
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du är för ung");
+
+            } 
+             if (ålder >19)  Console.WriteLine("Du är för gammal");
+               
+            
+           
+        {
+              
+             
+            
+        } 
+        
+        } 
+         
+    }
+}
